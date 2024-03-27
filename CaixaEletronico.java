3c7b22bf35a15b85b2ac2389fbb73db41a44dded
majucorrea07 <159278297+majucorrea07@users.noreply.github.com>
2024-03-27 11:08:05 -0300

    alteração do exercicio

--- CaixaEletronico.java
+++ CaixaEletronico.java
@@ -6,7 +6,7 @@ public class CaixaEletronico {
 		// TODO Auto-generated method stub
 		Scanner ler = new Scanner (System.in);
 		String cpf,senha;
-		double valor,sf;
+		double valor,sf,saldo=3000;
 
 		System.out.println("Digite seu CPF: ");
 		cpf = ler.nextLine();
@@ -15,11 +15,11 @@ public class CaixaEletronico {
 			System.out.println("Digite sua senha");
 			senha = ler.nextLine();
 			if (senha.equals("091722")) {
-				System.out.println("Saldo atual é correspondente a 3000");
+				System.out.println("Saldo atual é correspondente a " + saldo);
 				System.out.println("Que valor você deseja sacar?");
 				valor = ler.nextDouble();
-				if (valor <=3000) {
-					sf= 3000 - valor;
+				if (valor <=saldo) {
+					sf= saldo - valor;
 					System.out.println("Seu saldo foi atualizado, valor correspondete é " + sf);
 				}else {
 					System.out.println("Saldo insuficiente");
